ad357834ee57a7c389ffe46d048c92585806a64c
ranaanjel <anjelranawork@gmail.com>
2025-05-11 17:20:55 +0530

    initialized helper files, routes and folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,26 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Lusitana, DM_Sans } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const inter = Inter({
+  "subsets":[
+    "latin"
+  ], 
+  weight:["200", "500","800"]
+})
+
+const dm_sans = DM_Sans({
+  "subsets":[
+    "latin"
+  ], 
+  weight:["200", "500","800"]
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -25,7 +40,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dm_sans.className} antialiased`}
       >
         {children}
       </body>
